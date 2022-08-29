fea2352d2ddd6dbf1cc2e8957fe1900c322453b9
dudubarbaro <eduardobarbaro.ifc@gmail.com>
2022-08-29 09:14:48 -0300

    gdzn

---
+++ src/components/template/FooterComp.vue
@@ -0,0 +1,33 @@
+<template>
+  <footer>
+    <b-container id="footer" class="bv-example-row">
+      <b-row>
+        <b-col class="footer1">
+          2022 Barbaro´s Admin S.A - Instituição de Administração. <br />
+          BR-280 - Colégio Agrícola, Araquari - SC, 89245-000 <br
+        /></b-col>
+        <b-col class="footer2">
+          <br />
+          Fale comigo. <br />
+          <br />
+          (47) 98472-6333 <br />
+          eduardobarbaro.ifc@gmail.com.br <br />
+          Atendimento 24h
+        </b-col>
+      </b-row>
+    </b-container>
+  </footer>
+</template>
+<style>
+footer {
+  background-color: #14213d;
+  height: 50%;
+  font-size: 1.3rem;
+  padding-left: 2rem;
+  color: #fca311;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  font-family: Arial, Helvetica, sans-serif;
+}
+</style>
